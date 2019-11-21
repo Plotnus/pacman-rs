26a2c2987294c68d47c81cfda0fe1e81b95aefe6
plot <plotnus@gmail.com>
2019-11-20 21:59:25 -0500

    open opengl canvas

--- src/main-gl.rs
+++ src/main-gl.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello openGL");
+extern crate sdl2;
+
+use std::result::Result;
+use std::string::String;
+
+fn main() -> Result<(),String> {
+    const SCREEN_WIDTH: u32 = 1280;
+    const SCREEN_HEIGHT: u32 = 720;
+
+    let sdl_context = sdl2::init()?;
+    let sdl_video = sdl_context.video()?;
+    let sdl_window = sdl_video.window("gl", SCREEN_WIDTH, SCREEN_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .map_err(|e| e.to_string())?;
+    
+    let mut canvas = sdl_window.into_canvas().build().map_err(|e| e.to_string())?;
+    canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+
+    let mut events = sdl_context.event_pump()?;
+
+    'main: loop {
+
+        for event in events.poll_iter() {
+
+            match event {
+                sdl2::event::Event::Quit{..} => break 'main,
+                _ => {},
+            }
+        }
+    }
+
+    Ok(())
 }
