fdbc01b59b0bbddaff2256abd8e723e6ae1b51e0
Jonathan Plotner <plotnus@gmail.com>
2020-09-07 08:17:53 -0700

    removed piston

--- src/main-gl.rs
+++ src/main-gl.rs
@@ -3,37 +3,38 @@ extern crate sdl2;
 use std::result::Result;
 use std::string::String;
 
-fn main() -> Result<(),String> {
+fn main() -> Result<(), String> {
     const SCREEN_WIDTH: u32 = 1280;
     const SCREEN_HEIGHT: u32 = 720;
 
     let sdl_context = sdl2::init()?;
     let sdl_video = sdl_context.video()?;
-    let sdl_window = sdl_video.window("gl", SCREEN_WIDTH, SCREEN_HEIGHT)
+    let sdl_window = sdl_video
+        .window("gl", SCREEN_WIDTH, SCREEN_HEIGHT)
         .position_centered()
         .opengl()
         .build()
         .map_err(|e| e.to_string())?;
-    
-    let mut canvas = sdl_window.into_canvas().build().map_err(|e| e.to_string())?;
+
+    let mut canvas = sdl_window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())?;
     canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
     canvas.clear();
     canvas.present();
 
-
     let mut events = sdl_context.event_pump()?;
 
     'main: loop {
-
         for event in events.poll_iter() {
-
             match event {
-                sdl2::event::Event::Quit{..}
-                  => break 'main,
-                sdl2::event::Event::KeyDown { keycode: Some(sdl2::keyboard::Keycode::Escape), ..}
-                  => break 'main,
-                _
-                  => {},
+                sdl2::event::Event::Quit { .. } => break 'main,
+                sdl2::event::Event::KeyDown {
+                    keycode: Some(sdl2::keyboard::Keycode::Escape),
+                    ..
+                } => break 'main,
+                _ => {}
             }
         }
     }
--- src/main-sdl2.rs
+++ src/main-sdl2.rs
@@ -6,7 +6,6 @@ use crate::board::*;
 mod vec2;
 type Vec2 = cgmath::Vector2<f32>;
 
-
 fn main() -> std::result::Result<(), std::string::String> {
     let cache_line_size = sdl2::cpuinfo::cpu_cache_line_size();
     let num_cpus = sdl2::cpuinfo::cpu_count();
@@ -18,16 +17,21 @@ fn main() -> std::result::Result<(), std::string::String> {
     let mut event_pump = sdl_context.event_pump()?;
 
     let video_subsystem = sdl_context.video()?;
-    let window = video_subsystem.window("fun times", 1280, 720)
+    let window = video_subsystem
+        .window("fun times", 1280, 720)
         .position_centered()
         .build()
         .unwrap();
-    let _image_context = sdl2::image::init(sdl2::image::InitFlag::PNG | sdl2::image::InitFlag::JPG)?;
-    let mut canvas = window.into_canvas().software().build().map_err(|e| e.to_string())?;
+    let _image_context =
+        sdl2::image::init(sdl2::image::InitFlag::PNG | sdl2::image::InitFlag::JPG)?;
+    let mut canvas = window
+        .into_canvas()
+        .software()
+        .build()
+        .map_err(|e| e.to_string())?;
 
     let target_fps = 60.0;
     let target_frame_duration = std::time::Duration::from_secs_f64(1.0 / target_fps);
-
 
     // Section: GameSpecific
     let board = Board::new();
@@ -37,10 +41,22 @@ fn main() -> std::result::Result<(), std::string::String> {
 
         for event in event_pump.poll_iter() {
             match event {
-                sdl2::event::Event::Quit {..} => {println!("Event::Quit"); break 'mainloop},
-                sdl2::event::Event::AppTerminating {..} => {dbg!(event);},
-                sdl2::event::Event::Window { win_event, ..} => {dbg!(win_event);},
-                sdl2::event::Event::KeyDown{ keycode: Some(keycode), .. } => {dbg!(keycode);},
+                sdl2::event::Event::Quit { .. } => {
+                    println!("Event::Quit");
+                    break 'mainloop;
+                }
+                sdl2::event::Event::AppTerminating { .. } => {
+                    dbg!(event);
+                }
+                sdl2::event::Event::Window { win_event, .. } => {
+                    dbg!(win_event);
+                }
+                sdl2::event::Event::KeyDown {
+                    keycode: Some(keycode),
+                    ..
+                } => {
+                    dbg!(keycode);
+                }
                 _ => {}
             }
         }
@@ -53,18 +69,19 @@ fn main() -> std::result::Result<(), std::string::String> {
         if frame_start_time.elapsed() > target_frame_duration {
             let elapsed_time = frame_start_time.elapsed().as_secs_f64();
             let target_time = target_frame_duration.as_secs_f64();
-            println!("WARNING: OVER TIME BUDGET BY {:.2}%)" ,(elapsed_time / target_time) - 1.0);
+            println!(
+                "WARNING: OVER TIME BUDGET BY {:.2}%)",
+                (elapsed_time / target_time) - 1.0
+            );
         } else {
             // waste time
             // reason added: this is much more accurate than a call to `std::thread::sleep`
             // a goal of this engine is a high level of precision on per frame updates
-            while frame_start_time.elapsed() < target_frame_duration {
-            }
+            while frame_start_time.elapsed() < target_frame_duration {}
         }
 
         println!("FPS: {:.2}", 1.0 / frame_start_time.elapsed().as_secs_f64());
         dbg!(frame_start_time.elapsed().as_micros());
-        
     }
 
     Ok(())
@@ -73,14 +90,11 @@ fn main() -> std::result::Result<(), std::string::String> {
 fn render_board(canvas: &mut sdl2::render::Canvas<sdl2::video::Window>, board: &Board) {
     const PX_WIDE: u32 = 8;
     const PX_HIGH: u32 = 8;
-    canvas.info().max_texture_width()
     for h in 0..board.num_tiles {
         if !board.tile_is_traversable(h) {
             let pos = board.get_local_pos_of_tile(h);
-            canvas.set_draw_color(sdl2::pixels::Color::RGB(0,0,150));
-            let rect = sdl2::rect::Rect::new(
-                pos.x as i32, pos.y as i32, PX_WIDE, PX_HIGH
-            );
+            canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 150));
+            let rect = sdl2::rect::Rect::new(pos.x as i32, pos.y as i32, PX_WIDE, PX_HIGH);
             canvas.fill_rect(rect).unwrap();
         }
     }
